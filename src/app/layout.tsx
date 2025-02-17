b58778d629a56cd86e3df17b2838dc7b632822eb
weeezik <mkihanya.33@gmail.com>
2025-02-17 13:33:09 -0800

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from '@/components/Providers';
+import { Analytics } from "@vercel/analytics/react"
 
 const geist = Geist({
   subsets: ["latin"],
